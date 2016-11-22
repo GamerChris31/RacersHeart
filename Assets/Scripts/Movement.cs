aa42149f2dc811bd4a4e409aa391737bb356e896
GamerChris31 <student@gamelab-imac-4.hostos.cuny.edu>
2016-11-22 15:18:37 -0500

    V2 of racer's heart

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	Animator myAnimator;
+	int animState;
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			animState = 1;
+		}
+		myAnimator.SetInteger ("cS", animState);
+
+	}
+}
